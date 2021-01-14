b5e2e5ab1d602f9d13cf43fd148113c024feb637
Taufiq Maulana F <taufiqmaulanaf@gmail.com>
2021-01-14 11:44:19 +0700

    Commit Tubes WAD

---
+++ home.php
@@ -0,0 +1,154 @@
+<div class="intro">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="section_title text-center">
+            <div>Welcome</div>
+            <h1>Amazing Hotel in front of the Sea</h1>
+          </div>
+        </div>
+      </div>
+      <div class="row intro_row">
+        <div class="col-xl-8 col-lg-10 offset-xl-2 offset-lg-1">
+          <div class="intro_text text-center">
+            <p>Big show</p>
+          </div>
+        </div>
+      </div>
+      <div class="row gallery_row">
+        <div class="col">
+
+         
+              
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Rooms -->
+
+  <div class="rooms_right container_wrapper">
+    <div class="container">
+      <div class="row row-eq-height">
+
+        <!-- Rooms Image -->
+        <div class="col-xl-6 order-xl-1 order-2">
+          <div class="rooms_slider_container">
+            <div class="owl-carousel owl-theme rooms_slider">
+              
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
+              </div>
+
+              
+            </div>
+          </div>
+        </div>
+
+        <!-- Rooms Content -->
+        <div class="col-xl-6 order-xl-2 order-1">
+          <div class="rooms_right_content">
+            <div class="section_title">
+              <div>Rooms</div>
+              <h1>Luxury Double Suite</h1>
+            </div>
+            <div class="rooms_text">
+              <p>Maecenas sollicitudin tincidunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperd iet leo eget tortor dictum, eget varius eros sagittis. Curabitur tempor dignissim massa ut faucibus sollicitudin tinci dunt maximus. Morbi tempus malesuada erat sed pellentesque.</p>
+            </div>
+            <div class="rooms_list">
+              <ul>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Morbi tempus malesuada erat sed</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Tempus malesuada erat sed</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Pellentesque vel neque finibus elit</span>
+                </li>
+              </ul>
+            </div>
+            <div class="rooms_price">$129/<span>Night</span></div>
+           
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- Rooms -->
+
+  <div class="rooms_left container_wrapper">
+    <div class="container">
+      <div class="row row-eq-height">
+        
+        <!-- Rooms Content -->
+        <div class="col-xl-6">
+          <div class="rooms_left_content">
+            <div class="section_title">
+              <div>Rooms</div>
+              <h1>Luxury Single Room</h1>
+            </div>
+            <div class="rooms_text">
+              <p>Maecenas sollicitudin tincidunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperd iet leo eget tortor dictum, eget varius eros sagittis. Curabitur tempor dignissim massa ut faucibus sollicitudin tinci dunt maximus. Morbi tempus malesuada erat sed pellentesque.</p>
+            </div>
+            <div class="rooms_list">
+              <ul>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Morbi tempus malesuada erat sed</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Tempus malesuada erat sed</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Pellentesque vel neque finibus elit</span>
+                </li>
+              </ul>
+            </div>
+            <div class="rooms_price">$89/<span>Night</span></div>
+           
+          </div>
+        </div>
+
+        <!-- Rooms Image -->
+        <div class="col-xl-6">
+          <div class="rooms_slider_container">
+            <div class="owl-carousel owl-theme rooms_slider">
+              
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
+              </div>
+
+              
+              
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- Discover -->
+
+  <div class="discover">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
